26a537b4b713df49f4fd7c8bdbe4102c2abc0fff
smedsrun <of.smedsrud@gmx.com>
2021-11-30 16:06:15 +0100

    dato vises i My Todos

--- public/scripts/ToDoList/publicLists.js
+++ public/scripts/ToDoList/publicLists.js
@@ -1,36 +1,35 @@
-async function publicLists(){
-    const container = document.getElementById('publicContainer');
+async function publicLists() {
+  const container = document.getElementById("publicContainer");
 
-    try{
-        const data = await getContentUnderCategory();
-        for(let category of data[0]){
-            const div = document.createElement('div');
-            div.className = "listcard";
-            const header = document.createElement('h2');
-            const publisher = document.createElement('p');
-            publisher.innerHTML = `Published by: ${category.username}`;
-            header.innerHTML = category.name;
-            div.appendChild(header);
-            for(let items of data[1]){
-                if(category.id === items.categoryid){
-                    const p = document.createElement('p');
-                    const divContent = document.createElement('div');
-                    divContent.className = "contentDiv";
-                    const check = document.createElement('p');
-                    if(items.done === true){
-                        check.innerHTML = "✔";
-                    }
-                    p.innerHTML = items.content;
-                    divContent.appendChild(p);
-                    divContent.appendChild(check);
-                    div.appendChild(divContent);
-                }
-            }
-            div.appendChild(publisher);
-            container.appendChild(div);
+  try {
+    const data = await getContentUnderCategory();
+    for (let category of data[0]) {
+      const div = document.createElement("div");
+      div.className = "listcard";
+      const header = document.createElement("h2");
+      const publisher = document.createElement("p");
+      publisher.innerHTML = `Published by: ${category.username}`;
+      header.innerHTML = category.name;
+      div.appendChild(header);
+      for (let items of data[1]) {
+        if (category.id === items.categoryid) {
+          const p = document.createElement("p");
+          const divContent = document.createElement("div");
+          divContent.className = "contentDiv";
+          const check = document.createElement("p");
+          if (items.done === true) {
+            check.innerHTML = "✔";
+          }
+          p.innerHTML = items.content;
+          divContent.appendChild(p);
+          divContent.appendChild(check);
+          div.appendChild(divContent);
         }
+      }
+      div.appendChild(publisher);
+      container.appendChild(div);
     }
-    catch(err){
-        console.log(err);
-    }
+  } catch (err) {
+    console.log(err);
+  }
 }
