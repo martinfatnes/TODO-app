498298786d79b25745c40e3222c1f382b090ac34
smedsrun <of.smedsrud@gmx.com>
2021-11-30 12:16:17 +0100

    setDate funksjoner

--- public/scripts/ToDoList/modify.js
+++ public/scripts/ToDoList/modify.js
@@ -1,161 +1,187 @@
 //DELETE FUNCTIONS_____________________________________
 
-async function deleteCategory(id){
-    const url = `/api/category/${id}`;
-    const token = localStorage.getItem('token');
+async function deleteCategory(id) {
+  const url = `/api/category/${id}`;
+  const token = localStorage.getItem("token");
 
-    const cfg = {
-        method: "DELETE",
-        headers: {
-            "content-type":"application/json",
-            "autorization": token
-        }
-    }
+  const cfg = {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+      autorization: token,
+    },
+  };
 
-    try{
-        const respons = await fetch(url, cfg);
-        const data = await respons.json();
+  try {
+    const respons = await fetch(url, cfg);
+    const data = await respons.json();
 
-        if(respons.status != 200){
-            throw "Coult not delete";
-        }
-        else{
-            console.log(data);
-        }
+    if (respons.status != 200) {
+      throw "Coult not delete";
+    } else {
+      console.log(data);
     }
-    catch(err){
-        console.log(err);
-    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function deleteContent(id){
-    const url = "/api/delete/content";
-    const token = localStorage.getItem('token');
-    
-    const updata = {
-        id: id
+async function deleteContent(id) {
+  const url = "/api/delete/content";
+  const token = localStorage.getItem("token");
+
+  const updata = {
+    id: id,
+  };
+
+  const cfg = {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+      autorization: token,
+    },
+    body: JSON.stringify(updata),
+  };
+
+  try {
+    const respons = await fetch(url, cfg);
+    const data = await respons.json();
+
+    if (respons.status != 200) {
+      throw "Could not find";
     }
 
-    const cfg = {
-        method: "DELETE",
-        headers: {
-            "content-type":"application/json",
-            "autorization": token
-        },
-        body: JSON.stringify(updata) 
-    }
-
-    try{
-        const respons = await fetch(url, cfg);
-        const data = await respons.json();
-
-        if(respons.status != 200){
-            throw "Could not find"
-        }
-
-        refresh();
-        return data;
-    }
-    catch(err){
-        console.log(err);
-    }
+    refresh();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 //UPDATE FUNCTIONS_______________________________
-async function updateContent(content, id){
-    const url = "/api/updateContent";
-    
-    const updata = {
-        content: content,
-        id: id
-    }
+async function setDate(categoryId, date) {
+  const url = "/api/setDate";
 
-    const cfg = {
-        method: "PUT",
-        headers: {
-            "content-type":"application/json"
-        },
-        body: JSON.stringify(updata) 
-    }
-
-    try{
-        const respons = await fetch(url, cfg);
-        const data = await respons.json();
-
-        if(respons.status != 200){
-            throw "Could not find"
-        }
-
-        refresh();
-        return data;
-    }
-    catch(err){
-        console.log(err);
-    }
-}
-
-async function updateCompletedItems(id, status){
-    const url = "/api/update/complteded";
-    
-    const updata = {
-        id: id,
-        status: status
-    }
-
-    const cfg = {
-        method: "PUT",
-        headers: {
-            "content-type":"application/json"
-        },
-        body: JSON.stringify(updata) 
-    }
-
-    try{
-        const respons = await fetch(url, cfg);
-        const data = await respons.json();
-
-        if(respons.status != 200){
-            throw "Could not update"
-        }
-
-        refresh();
-        return data;
-    }
-    catch(err){
-        console.log(err);
-    }
-}
-
-async function updateTag(id, tag){
-  const url = '/api/tags';
-  console.log("shit");
   const updata = {
-      id: id,
-      tag: tag
-  }
+    content: date,
+    id: categoryId,
+  };
 
   const cfg = {
     method: "PUT",
     headers: {
-        "content-type":"application/json"
+      "content-type": "application/json",
     },
-    body: JSON.stringify(updata) 
+    body: JSON.stringify(updata),
+  };
+  try {
+    const respons = await fetch(url, cfg);
+    const data = await respons.json();
+    s;
+
+    if (respons.status != 200) {
+      throw "Could not find";
+    }
+
+    refresh();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+setDate(345, Date.now());
+
+async function updateContent(content, id) {
+  const url = "/api/updateContent";
+
+  const updata = {
+    content: content,
+    id: id,
+  };
+
+  const cfg = {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(updata),
+  };
+
+  try {
+    const respons = await fetch(url, cfg);
+    const data = await respons.json();
+
+    if (respons.status != 200) {
+      throw "Could not find";
+    }
+
+    refresh();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-    try{
-        const respons = await fetch(url, cfg);
-        const data = await respons.json();
+async function updateCompletedItems(id, status) {
+  const url = "/api/update/complteded";
 
-        if(respons.status != 200){
-            throw "Could not update"
-        }
+  const updata = {
+    id: id,
+    status: status,
+  };
 
-        refresh();
-        return data;
+  const cfg = {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(updata),
+  };
+
+  try {
+    const respons = await fetch(url, cfg);
+    const data = await respons.json();
+
+    if (respons.status != 200) {
+      throw "Could not update";
     }
-    catch(err){
-        console.log(err);
+
+    refresh();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function updateTag(id, tag) {
+  const url = "/api/tags";
+  console.log("shit");
+  const updata = {
+    id: id,
+    tag: tag,
+  };
+
+  const cfg = {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(updata),
+  };
+
+  try {
+    const respons = await fetch(url, cfg);
+    const data = await respons.json();
+
+    if (respons.status != 200) {
+      throw "Could not update";
     }
+
+    refresh();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 updateTag(196, "crazy");
--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-const express = require('express');
+const express = require("express");
 const users = require("./modules/login/users");
-const items = require('./modules/APP/content');
-const category = require('./modules/APP/category');
+const items = require("./modules/APP/content");
+const category = require("./modules/APP/category");
 const server = express();
 const PORT = process.env.PORT || 8080;
 
-server.use(express.static('public'));
+server.use(express.static("public"));
 server.set("PORT", PORT);
 server.use(express.json());
 server.use((err, req, res, next) => {
-    res.status(500).json({
-        error: "Something bad on the server",
-        desc: err
-    }).end();
-})
+  res
+    .status(500)
+    .json({
+      error: "Something bad on the server",
+      desc: err,
+    })
+    .end();
+});
 server.use(users);
 server.use(items);
 server.use(category);
 
-server.listen(server.get('PORT'), function(){
-    console.log("server running", server.get("PORT"));
-})
+server.listen(server.get("PORT"), function () {
+  console.log("server running", server.get("PORT"));
+});
