d989636fb20b3fe591dd7426333faacd8c5e9773
VegardGronas <92518188+VegardGronas@users.noreply.github.com>
2021-12-01 09:41:43 +0100

    Søke på tag

--- public/scripts/ToDoList/publicLists.js
+++ public/scripts/ToDoList/publicLists.js
@@ -35,12 +35,13 @@ function test(data, filter){
 }
 
 ______________________________________________________*/
-async function publicLists(){
+function publicLists(data, filter){
     const container = document.getElementById('publicContainer');
-    const searchbar = document.getElementById("searchbar");
-    let tagSearch = [];
+    container.innerHTML = "";
+    //const searchbar = document.getElementById("searchbar");
+    //const tagSearch = [];
 
-    
+    /*
     searchbar.addEventListener('keyup', (e) => {
             const searchString = e.target.value;
             const filteredTags = tagSearch.filter( category =>{
@@ -48,13 +49,44 @@ async function publicLists(){
         });
             console.log(filteredTags);
     });
-    
-
-    try{
+    */
         
-        for(let category of data[0]){
-            tagSearch.push(category.tag);
-            console.log(tagSearch);
+    for(let category of data[0]){
+        if(filter){
+            for(let fil of filter){
+                if(fil === category.tag){
+                    const div = document.createElement('div');
+                    div.className = "listcard";
+                    const header = document.createElement('h2');
+                    const tagParagraph = document.createElement('p');
+                    const publisher = document.createElement('p');
+                    publisher.innerHTML = `Published by: ${category.username}`;
+                    header.innerHTML = category.name;
+                    tagParagraph.innerHTML += "#" + category.tag;
+                    div.appendChild(header);
+                        
+                    for(let items of data[1]){
+                        if(category.id === items.categoryid){
+                            const p = document.createElement('p');
+                            const divContent = document.createElement('div');
+                            divContent.className = "contentDiv";
+                            const check = document.createElement('p');
+                            if(items.done === true){
+                                check.innerHTML = "✔";
+                            }
+                            p.innerHTML = items.content;
+                            divContent.appendChild(p);
+                            divContent.appendChild(check);
+                            div.appendChild(divContent);
+                        }
+                    }
+                    div.appendChild(publisher);
+                    div.appendChild(tagParagraph);
+                    container.appendChild(div);
+                }
+            }
+        }
+        else{
             const div = document.createElement('div');
             div.className = "listcard";
             const header = document.createElement('h2');
@@ -64,7 +96,7 @@ async function publicLists(){
             header.innerHTML = category.name;
             tagParagraph.innerHTML += "#" + category.tag;
             div.appendChild(header);
-            
+                
             for(let items of data[1]){
                 if(category.id === items.categoryid){
                     const p = document.createElement('p');
@@ -83,20 +115,42 @@ async function publicLists(){
             div.appendChild(publisher);
             div.appendChild(tagParagraph);
             container.appendChild(div);
-            div.appendChild(publisher);
         }
-      }
-    catch (err) {
-    console.log(err);
     }
 }
 
 async function getPublicLists(){
     try{
-
+        const data = await getContentUnderCategory();
+        publicLists(data);
+        searchForTags(data);
     }
     catch(err){
         console.log(err);
     }
-    const data = await getContentUnderCategory();
+}
+
+function searchForTags(data){
+    const searchbar = document.getElementById("searchbar");
+    const tagSearch = [];
+
+    for(let value of data[0]){
+        tagSearch.push(value.tag);
+    }
+
+    searchbar.addEventListener('keyup', (e) => {
+            const searchString = e.target.value;
+            const filteredTags = tagSearch.filter( category =>{
+            return category.includes(searchString);
+        });
+
+        for(let filter of filteredTags){
+            for(let value of data[0]){
+                if(filter === value.tag){
+                    publicLists(data, filteredTags);
+                }
+            }
+        }
+    });
+    
 }
